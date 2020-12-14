3b0eb922927f092cb9de574160e58aa0f214ffee
Sam Williams <c3207769@uon.edu.au>
2020-12-14 23:30:22 +1100

    move plot specific stuff to new 'plot.py' file

--- LASplot.py
+++ LASplot.py
@@ -5,6 +5,7 @@ from laspy.file import File
 import numpy as np
 import matplotlib.pyplot as plt
 import time
+import plot
 
 # initialise window
 root = Tk()
@@ -21,7 +22,7 @@ composite_var = IntVar()
 # window settings
 root.title("LASplot")
 root.geometry("900x500")
-root.minsize(500, 350)
+root.minsize(700, 400)
 root.rowconfigure(0, weight=1)
 root.columnconfigure(0, weight=1)
 
@@ -32,8 +33,15 @@ def main():
     # icon = PhotoImage(file="icon.png")
     # Label(root, image=icon, background="white").grid(row=2, column=1, sticky=N)
 
+    # main control panel at the top of the form
+    top_panel = LabelFrame(root)
+    top_panel.rowconfigure(0, weight=1)
+    top_panel.columnconfigure(0, weight=1)
+    top_panel.grid(row=0, column=0, columnspan=2, sticky=N + W + E)
+
+    # main frame containing ou
     # ---------------------------- CHOOSE INPUT / OUTPUT ----------------------------
-    file_frame = LabelFrame(root, text="user input/ output")
+    file_frame = LabelFrame(top_panel, text="user input/ output")
     file_frame.rowconfigure(0, weight=1)
     file_frame.columnconfigure(0, weight=1)
     file_frame.grid(row=0, column=0, columnspan=2, sticky=N + W + E, padx=10, pady=10)
@@ -58,29 +66,29 @@ def main():
 
     # label to display chosen file
     input_lbl = Label(file_frame, textvariable=source_var)
-    input_lbl.grid(row=0, column=1, sticky=E)
+    input_lbl.grid(row=0, column=1, sticky=E, padx=5, pady=5)
 
     # label to display chosen destination
     output_lbl = Label(file_frame, textvariable=destination_var)
-    output_lbl.grid(row=1, column=1, sticky=E)
+    output_lbl.grid(row=1, column=1, rowspan=3, sticky=E, padx=5, pady=5)
 
     # ---------------------------- CHOOSE IMAGE SETTINGS ----------------------------
-    options_frame = LabelFrame(root, text="image options")
+    options_frame = LabelFrame(top_panel, text="image options")
     options_frame.rowconfigure(0, weight=1)
     options_frame.columnconfigure(0, weight=1)
-    options_frame.grid(row=3, column=0, sticky=W + S, padx=10, pady=10)
+    options_frame.grid(row=0, column=2, sticky=E, padx=10, pady=10)
 
     dpi_label = Label(options_frame, text="DPI")
-    dpi_label.grid(row=3, column=0, sticky=W + E, padx=10, pady=10)
+    dpi_label.grid(row=3, column=0, sticky=W + E, padx=5, pady=5)
 
     dpi_input = Entry(options_frame)
-    dpi_input.grid(row=3, column=1, sticky=W + E, padx=10, pady=10)
+    dpi_input.grid(row=3, column=1, sticky=W + E, padx=5, pady=5)
 
     size_label = Label(options_frame, text="SIZE")
-    size_label.grid(row=4, column=0, sticky=W + E, padx=10, pady=10)
+    size_label.grid(row=4, column=0, sticky=W + E, padx=5, pady=5)
 
     size_label = Entry(options_frame)
-    size_label.grid(row=4, column=1, sticky=W + E, padx=10, pady=10)
+    size_label.grid(row=4, column=1, sticky=W + E, padx=5, pady=5)
 
     # ---------------------------- CHOOSE OUTPUT SETTINGS ----------------------------
     # frame to contain/ group user controls
@@ -124,7 +132,7 @@ def main():
         padx=10,
         pady=10,
     )
-    begin_btn.grid(row=1, column=1, sticky=SE, padx=10, pady=10)
+    begin_btn.grid(row=2, column=1, sticky=SE, padx=10, pady=10)
 
     # run main window
     root.mainloop()
@@ -145,13 +153,6 @@ def choose_dest():
     destination_var.set(filedialog.askdirectory())
 
 
-# get the positional data of points in a specified classification
-def get_xy(in_points, classification):
-    x = in_points.X[in_points.Classification == classification]
-    y = in_points.Y[in_points.Classification == classification]
-    return x, y
-
-
 def valid_inputs():
     if source_var.get() != "" and destination_var.get() != "":
         return True
@@ -160,89 +161,13 @@ def valid_inputs():
 def handler():
     if valid_inputs():
         if plot_var.get() == 1:
-            plot()
+            plot.plot(source_var.get(), destination_var.get(), 100, 20)
         if contour_var.get() == 1:
             print("do contour stuff....")
         if composite_var.get() == 1:
             print("do composite stuff....")
     else:
         print("please select correct input values")
-
-
-# plot the positional data and then save as PNG
-def plot():
-    size = 100
-    dpi = 20
-
-    # read in LAS file and specify point records, las spec
-    input_file = File(source_var.get(), mode="r")
-    point_records = input_file.points
-    las_specification = input_file.point_format.fmt
-
-    # get the min/max X,Y values to normalise the plot scale
-    x_min, x_max = np.amin(input_file.X), np.amax(input_file.X)
-    y_min, y_max = np.amin(input_file.Y), np.amax(input_file.Y)
-
-    # print output of derived header information
-    print_header_info(input_file, point_records, las_specification)
-
-    # initialise point-variable arrays
-    buildings = get_xy(input_file, 6)
-    unclassified = get_xy(input_file, 1)
-    ground = get_xy(input_file, 2)
-    lowVeg = get_xy(input_file, 3)
-    medVeg = get_xy(input_file, 4)
-    highVeg = get_xy(input_file, 5)
-    water = get_xy(input_file, 9)
-
-    # basic params for the plot function
-    plt.rcParams["figure.figsize"] = [size, size]
-    plt.rcParams["figure.facecolor"] = "black"
-
-    print("")
-    print("CLASSIFICATION PLOT")
-    print("-----------------------------------------")
-
-    const_args = dpi, x_min, x_max, y_min, y_max
-
-    # save the individual layer plots as .PNG
-    save(*unclassified, "/unclassified.png", "m", *const_args)
-    save(*ground, "/ground.png", "SaddleBrown", *const_args)
-    save(*lowVeg, "/lowVeg.png", "LimeGreen", *const_args)
-    save(*medVeg, "/mediumVeg.png", "Green", *const_args)
-    save(*highVeg, "/highVeg.png", "DarkGreen", *const_args)
-    save(*buildings, "/buildings.png", "White", *const_args)
-    save(*water, "/water.png", "DodgerBlue", *const_args)
-
-    print("-----------------------------------------")
-    print("process complete")
-
-
-# save the plotted images
-def save(x_, y_, filename_, color_, dpi, x_min, x_max, y_min, y_max):
-    plt.plot(x_, y_, color=color_, linestyle="none", marker=",")
-    plt.xlim(x_min, x_max)
-    plt.ylim(y_min, y_max)
-    plt.margins(0, 0)
-    plt.gca().set_facecolor("black")
-    fig = plt.gcf()
-    fig.savefig(
-        destination_var.get() + filename_,
-        dpi=dpi,
-        bbox_inches="tight",
-        pad_inches=0,
-        facecolor="black",
-    )
-    plt.clf()
-    print(filename_, "saved successfully")
-
-
-def print_header_info(input_file, point_records, las_specification):
-    print("---------------------------HEADER INFORMATION--------------------------")
-    print("LAS specification = " + input_file.header.version)
-    print("point format = " + str(las_specification))
-    print("total point count = " + str(input_file.header.count))
-    print("-----------------------------------------------------------------------")
 
 
 if __name__ == "__main__":
---
+++ plot.py
@@ -0,0 +1,88 @@
+from laspy.file import File
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+# get the positional data of points in a specified classification
+def get_xy(in_points, classification):
+    x = in_points.X[in_points.Classification == classification]
+    y = in_points.Y[in_points.Classification == classification]
+    return x, y
+
+
+# plot the positional data and then save as PNG
+def plot(input, output, size, dpi):
+
+    # read in LAS file and specify point records, las spec
+    input_file = File(input, mode="r")
+    point_records = input_file.points
+    las_specification = input_file.point_format.fmt
+
+    # get the min/max X,Y values to normalise the plot scale
+    x_min, x_max = np.amin(input_file.X), np.amax(input_file.X)
+    y_min, y_max = np.amin(input_file.Y), np.amax(input_file.Y)
+
+    # print output of derived header information
+    print_header_info(input_file, point_records, las_specification)
+
+    # initialise point-variable arrays
+    buildings = get_xy(input_file, 6)
+    unclassified = get_xy(input_file, 1)
+    ground = get_xy(input_file, 2)
+    lowVeg = get_xy(input_file, 3)
+    medVeg = get_xy(input_file, 4)
+    highVeg = get_xy(input_file, 5)
+    water = get_xy(input_file, 9)
+
+    # basic params for the plot function
+    plt.rcParams["figure.figsize"] = [size, size]
+    plt.rcParams["figure.facecolor"] = "black"
+
+    print("")
+    print("CLASSIFICATION PLOT")
+    print("-----------------------------------------")
+
+    const_args = output, dpi, x_min, x_max, y_min, y_max
+
+    # save the individual layer plots as .PNG
+    save(*unclassified, "/unclassified.png", "m", *const_args)
+    save(*ground, "/ground.png", "SaddleBrown", *const_args)
+    save(*lowVeg, "/lowVeg.png", "LimeGreen", *const_args)
+    save(*medVeg, "/mediumVeg.png", "Green", *const_args)
+    save(*highVeg, "/highVeg.png", "DarkGreen", *const_args)
+    save(*buildings, "/buildings.png", "White", *const_args)
+    save(*water, "/water.png", "DodgerBlue", *const_args)
+
+    print("-----------------------------------------")
+    print("process complete")
+
+
+# save the plotted images
+def save(x_, y_, filename_, color_, output, dpi, x_min, x_max, y_min, y_max):
+    plt.plot(x_, y_, color=color_, linestyle="none", marker=",")
+    plt.xlim(x_min, x_max)
+    plt.ylim(y_min, y_max)
+    plt.margins(0, 0)
+    plt.gca().set_facecolor("black")
+    fig = plt.gcf()
+    fig.savefig(
+        output + filename_,
+        dpi=dpi,
+        bbox_inches="tight",
+        pad_inches=0,
+        facecolor="black",
+    )
+    plt.clf()
+    print(filename_, "saved successfully")
+
+
+def print_header_info(input_file, point_records, las_specification):
+    print("---------------------------HEADER INFORMATION--------------------------")
+    print("LAS specification = " + input_file.header.version)
+    print("point format = " + str(las_specification))
+    print("total point count = " + str(input_file.header.count))
+    print("-----------------------------------------------------------------------")
+
+
+if __name__ == "__main__":
+    main()
