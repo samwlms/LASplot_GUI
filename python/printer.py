74ced3d6e4ed8a574507f7d00357c6f432a0726f
Sam Williams <c3207769@uon.edu.au>
2020-12-21 04:04:08 +1100

    add some headings to the printer

--- python/printer.py
+++ python/printer.py
@@ -8,40 +8,68 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
-# plot the positional data and then save as PNG
-def test(input):
+def saved(filename):
+    print(filename, "saved successfully")
+
+
+def plot_print():
+
+    print("")
+    print("-----------------------------------------")
+    print("----------CLASSIFICATION PLOTS-----------")
+    print("-----------------------------------------")
+
+
+def contour_print():
+
+    print("")
+    print("-----------------------------------------")
+    print("--------------CONTOUR PLOT---------------")
+    print("-----------------------------------------")
+
+
+def gradient_print():
+
+    print("")
+    print("-----------------------------------------")
+    print("-------------GRADIENT PLOT---------------")
+    print("-----------------------------------------")
+
+
+def info_print():
+
+    print("")
+    print("-----------------------------------------")
+    print("----------------FILE INFO----------------")
+    print("-----------------------------------------")
+
+
+def complete():
+
+    print("-----------------------------------------")
+    print("process complete")
+
+
+def format(input):
 
     # read in LAS file and specify point records, las spec
     input_file = File(input, mode="r")
     point_records = input_file.points
     pointformat = input_file.point_format
 
-    print("")
-    print("-----------------------------------------")
-    print("----------------FILE INFO----------------")
-    print("-----------------------------------------")
-    print("")
+    info_print()
 
     print("------ Point data format: ------")
-
     pointformat = input_file.point_format
     for spec in pointformat:
         print(spec.name)
 
-    print("")
-
     print("------ Sample point data: ------")
-
     print(point_records[0])
 
-    print("")
-
     print("------ Header data format: ------")
-
     headerformat = input_file.header.header_format
     for spec in headerformat:
         print(spec.name)
 
-    print("")
-    print("-----------------------------------------")
-    print("process complete")
+    complete()
