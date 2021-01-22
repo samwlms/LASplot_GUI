1d3162fbe281a734ee50518211f1ba5e18b8ad2d
Sam Williams <c3207769@uon.edu.au>
2021-01-22 21:10:06 +1100

    add status indicator and header

--- install.bat
+++ install.bat
@@ -1,3 +1,4 @@
 @echo off
+ECHO ------------------------LASPLOT INSTALLATION------------------------------
+ECHO installing project dependencies and initialising virtual environment...
 call python -m venv env & env\Scripts\activate.bat & env\Scripts\pip.exe install -r requirements.txt
-pause
