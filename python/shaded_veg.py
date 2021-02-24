4628b3d7aae68f6325affa0c797bb25c7e3e4ada
Sam Williams <c3207769@uon.edu.au>
2021-02-24 15:41:37 +1100

    add python script for shaded veg

---
+++ python/shaded_veg.py
@@ -0,0 +1,8 @@
+# Written by: Sam Williams
+# Contact: Swilliams9@uon.edu.au
+# Project is open for use/ collaboration by all!
+
+from laspy.file import File
+import numpy as np
+import printer, time
+import matplotlib.pyplot as plt
