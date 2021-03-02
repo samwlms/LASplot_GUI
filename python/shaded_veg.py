302f898b80def738c2f70f688d15b1b5f93ef831
Sam Williams <c3207769@uon.edu.au>
2021-03-02 21:25:45 +1100

    use scaled z dimensions

--- python/shaded_veg.py
+++ python/shaded_veg.py
@@ -14,17 +14,23 @@ def plot_shaded(input):
     input_file = File(input, mode="r")
     veg = np.vstack(
         [
-            input_file.X[input_file.Classification == 5],
-            input_file.Y[input_file.Classification == 5],
-            input_file.Z[input_file.Classification == 5],
+            input_file.x[input_file.Classification == 5],
+            input_file.y[input_file.Classification == 5],
+            input_file.z[input_file.Classification == 5],
         ]
+    ).transpose()
 
+    ground = np.vstack(
+        [
+            input_file.x[input_file.Classification == 2],
+            input_file.y[input_file.Classification == 2],
+            input_file.z[input_file.Classification == 2],
+        ]
+    ).transpose()
 
-
-    ground=np.vstack([input_file.X[input_file.Classification == 2], input_file.Y[input_file.Classification ==
-                     2], input_file.Z[input_file.Classification == 2], ]).transpose()
-
-    a_veg_point=veg[100]
-    closest_point=ground[KDTree(ground).query(a_veg_point)[1]]
-    print("closest point", closest_point)
-    print(closest_point)
+    a_veg_point = veg[100]
+    closest_point = ground[KDTree(ground).query(a_veg_point)[1]]
+    print("a_veg_point:", a_veg_point)
+    print("closest point:", closest_point)
+    distance_from_ground = a_veg_point[2] - closest_point[2]
+    print("distance_from_ground:", distance_from_ground)
