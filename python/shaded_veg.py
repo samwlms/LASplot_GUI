704f04722e71f4b721f2f53ca4f652250f1312c6
Sam Williams <c3207769@uon.edu.au>
2021-03-03 00:17:39 +1100

    function to find veg point height from ground

--- python/shaded_veg.py
+++ python/shaded_veg.py
@@ -14,22 +14,34 @@ def plot_shaded(input):
     input_file = File(input, mode="r")
     veg = np.vstack(
         [
-            input_file.x[input_file.Classification == 5],
-            input_file.y[input_file.Classification == 5],
-            input_file.z[input_file.Classification == 5],
+            input_file.X[input_file.Classification == 5],
+            input_file.Y[input_file.Classification == 5],
+            input_file.Z[input_file.Classification == 5],
         ]
     ).transpose()
 
     ground = np.vstack(
         [
-            input_file.x[input_file.Classification == 2],
-            input_file.y[input_file.Classification == 2],
-            input_file.z[input_file.Classification == 2],
+            input_file.X[input_file.Classification == 2],
+            input_file.Y[input_file.Classification == 2],
+            input_file.Z[input_file.Classification == 2],
         ]
     ).transpose()
 
-    tree = KDTree(ground)
-    for point in veg:
-        closest_point = ground[tree.query(point)[1]]
-        distance_from_ground = point[2] - closest_point[2]
-        print("distance from ground:", distance_from_ground)
+    ground_tree = KDTree(ground)
+
+    heights = []
+
+    for count, point in enumerate(veg):
+        height = round(get_height(ground_tree, ground, point) * 0.01, 2)
+        heights.append(height)
+    max_height = np.amax(heights)
+    min_height = np.amin(heights)
+    print("max height:", max_height)
+    print("min height:", min_height)
+
+
+def get_height(ground_tree, ground, point):
+    closest_point = ground[ground_tree.query(point)[1]]
+    distance_from_ground = point[2] - closest_point[2]
+    return distance_from_ground
