37e6f07df69c9c5c6e1756bebe049e78098ff2c5
Sam Williams <c3207769@uon.edu.au>
2021-03-02 21:15:55 +1100

    find closest ground point for given veg point

--- python/shaded_veg.py
+++ python/shaded_veg.py
@@ -4,5 +4,27 @@
 
 from laspy.file import File
 import numpy as np
-import printer, time
+from scipy.spatial.kdtree import KDTree
+import printer
+import time
 import matplotlib.pyplot as plt
+
+
+def plot_shaded(input):
+    input_file = File(input, mode="r")
+    veg = np.vstack(
+        [
+            input_file.X[input_file.Classification == 5],
+            input_file.Y[input_file.Classification == 5],
+            input_file.Z[input_file.Classification == 5],
+        ]
+
+
+
+    ground=np.vstack([input_file.X[input_file.Classification == 2], input_file.Y[input_file.Classification ==
+                     2], input_file.Z[input_file.Classification == 2], ]).transpose()
+
+    a_veg_point=veg[100]
+    closest_point=ground[KDTree(ground).query(a_veg_point)[1]]
+    print("closest point", closest_point)
+    print(closest_point)
